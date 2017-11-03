015fae5e7a749b84d62702578b8557a58c6ee9f4
rhythmrice <ricerhythm@gmail.com>
2017-11-03 11:34:13 -0500

    more work

---
+++ Date String/Date String/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles lblDateString.Click
+
+    End Sub
+
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        ' Concatenate the input and build the date string.
+        lblDateString.Text = txtDayOfWeek.Text & ", " &
+            txtMonth.Text & " " &
+            txtDayOfMonth.Text & ", " &
+            txtYear.Text
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear the text boxes and lblDateString
+        txtDayOfWeek.Clear()
+        txtMonth.Clear()
+        txtDayOfMonth.Clear()
+        txtYear.Clear()
+        lblDateString.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close
+        Me.Close()
+    End Sub
+End Class
