49e117c67fa6b9e3444a8fa4eb1365fa6bd04e95
rhythmrice <ricerhythm@gmail.com>
2017-11-03 11:19:05 -0500

    update greetings

---
+++ greetings/greetings/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.lblGreeting = New System.Windows.Forms.Label()
+        Me.btnShowGreeting = New System.Windows.Forms.Button()
+        Me.Buttonexit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(110, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "enter your name"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(86, 65)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(100, 20)
+        Me.txtUserName.TabIndex = 1
+        '
+        'lblGreeting
+        '
+        Me.lblGreeting.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGreeting.Location = New System.Drawing.Point(49, 117)
+        Me.lblGreeting.Name = "lblGreeting"
+        Me.lblGreeting.Size = New System.Drawing.Size(191, 23)
+        Me.lblGreeting.TabIndex = 2
+        '
+        'btnShowGreeting
+        '
+        Me.btnShowGreeting.Location = New System.Drawing.Point(49, 185)
+        Me.btnShowGreeting.Name = "btnShowGreeting"
+        Me.btnShowGreeting.Size = New System.Drawing.Size(75, 38)
+        Me.btnShowGreeting.TabIndex = 3
+        Me.btnShowGreeting.Text = "show greeting"
+        Me.btnShowGreeting.UseVisualStyleBackColor = True
+        '
+        'Buttonexit
+        '
+        Me.Buttonexit.Location = New System.Drawing.Point(148, 185)
+        Me.Buttonexit.Name = "Buttonexit"
+        Me.Buttonexit.Size = New System.Drawing.Size(75, 23)
+        Me.Buttonexit.TabIndex = 4
+        Me.Buttonexit.Text = "exit"
+        Me.Buttonexit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Buttonexit)
+        Me.Controls.Add(Me.btnShowGreeting)
+        Me.Controls.Add(Me.lblGreeting)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents lblGreeting As System.Windows.Forms.Label
+    Friend WithEvents btnShowGreeting As System.Windows.Forms.Button
+    Friend WithEvents Buttonexit As System.Windows.Forms.Button
+
+End Class
---
+++ greetings/greetings/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub btnShowGreeting_Click(sender As Object, e As EventArgs) Handles btnShowGreeting.Click
+        Dim strName As String
+
+        strName = txtUserName.Text
+
+        lblGreeting.Text = "Hi " + strName
+    End Sub
+
+    Private Sub Buttonexit_Click(sender As Object, e As EventArgs) Handles Buttonexit.Click
+        Me.Close()
+    End Sub
+End Class
